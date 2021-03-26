7cc9bb8051544b28933b532b56524f9fca5c2bc7
ventlark <ventlark@gmail.com>
2021-03-26 21:56:12 +0000

    Define basic cli arguments

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,41 @@
 const std = @import("std");
+const clap = @import("clap");
+
+const params = comptime [_]clap.Param(clap.Help){
+    clap.parseParam("-h, --help     Display this help and exit") catch unreachable,
+    clap.parseParam("-l, --lang <STR> TLDR page language") catch unreachable,
+    clap.parseParam("-u, --update   Update TLDR pages") catch unreachable,
+    clap.parseParam("<POS>...") catch unreachable,
+};
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
+
+    var diag: clap.Diagnostic = undefined;
+    var args = clap.parse(clap.Help, &params, std.heap.page_allocator, &diag) catch |err| {
+        diag.report(std.io.getStdErr().outStream(), err) catch {};
+        try helpExit();
+        return err;
+    };
+    defer args.deinit();
+
+    if (args.flag("--help")) {
+        try helpExit();
+    }
+
+    if (args.flag("--update")) {
+        std.debug.print("TODO: implement updating pages", .{});
+        std.process.exit(0);
+    }
+}
+
+fn helpExit() !void {
+    const stderr = std.io.getStdErr().outStream();
+
+    try stderr.print("Usage: {} ", .{std.os.argv[0]});
+    try clap.usage(stderr, &params);
+    try stderr.print("\nFlags: \n", .{});
+    try clap.help(stderr, &params);
+
+    std.process.exit(1);
 }
