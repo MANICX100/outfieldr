d6ef4c642752706d24d5868628f2743c53911ac4
vent <ventlark@gmail.com>
2021-04-09 22:17:19 +0100

    Rename executable to tldr

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zealdr", "src/main.zig");
+    const exe = b.addExecutable("tldr", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
