a6421111bdcade7cb039d4d5b2754092f0f17c83
vent <ventlark@gmail.com>
2021-04-12 03:20:16 +0100

    Refactor main

--- src/main.zig
+++ src/main.zig
@@ -22,51 +22,56 @@ pub fn main() anyerror!void {
     };
     defer args.deinit();
 
-    const got_positional_args: bool = args.positionals().len > 0;
-
+    const stdout = std.io.getStdOut();
     var gpa = GeneralPurposeAllocator(.{}){};
     var allocator = &gpa.allocator;
 
-    const stdout = std.io.getStdOut();
+    const positionals: ?[]const []const u8 = pos: {
+        const pos = args.positionals();
+        break :pos if (pos.len > 0) pos else null;
+    };
 
     if (args.flag("--help")) {
         try helpExit();
     }
 
-    var fetched = false;
-    if (args.flag("--fetch")) {
-        try Pages.fetch(allocator);
-        if (!got_positional_args) std.process.exit(0);
-        _ = try stdout.writer().write("--\n");
-        fetched = true;
-    }
-
-    if (!got_positional_args) {
-        try helpExit();
-    }
-
-    var tldr_pages = Pages.open(args.option("--lang")) catch std.process.exit(1);
-    defer tldr_pages.close();
-
-    const page_contents = tldr_pages.pageContents(allocator, args.positionals()) catch |err| {
-        const stderr = std.io.getStdErr();
-        switch (err) {
-            error.FileNotFound => {
-                var msg = if (fetched)
-                    "Page doesn't exist in tldr-master. Consider contributing it!\n"
-                else
-                    "Page not found. Perhaps try with `--fetch`\n";
-
-                _ = try stderr.write(msg);
-                std.process.exit(1);
-            },
-            else => return err,
+    const fetched = f: {
+        if (args.flag("--fetch")) {
+            try Pages.fetch(allocator);
+            if (positionals == null) std.process.exit(0);
+            _ = try stdout.writer().write("--\n");
+            break :f true;
+        } else {
+            break :f false;
         }
     };
-    defer allocator.free(page_contents);
 
-    const pretty_contents = try pretty.prettify(allocator, page_contents);
-    _ = try stdout.writer().print("{s}\n", .{pretty_contents});
+    if (positionals) |pos| {
+        var tldr_pages = Pages.open(args.option("--lang")) catch std.process.exit(1);
+        defer tldr_pages.close();
+
+        const page_contents = tldr_pages.pageContents(allocator, pos) catch |err| {
+            const stderr = std.io.getStdErr();
+            switch (err) {
+                error.FileNotFound => {
+                    var msg = if (fetched)
+                        "Page doesn't exist in tldr-master. Consider contributing it!\n"
+                    else
+                        "Page not found. Perhaps try with `--fetch`\n";
+
+                    _ = try stderr.write(msg);
+                    std.process.exit(1);
+                },
+                else => return err,
+            }
+        };
+        defer allocator.free(page_contents);
+
+        const pretty_contents = try pretty.prettify(allocator, page_contents);
+        _ = try stdout.writer().print("{s}\n", .{pretty_contents});
+    } else {
+        try helpExit();
+    }
 }
 
 fn helpExit() !void {
