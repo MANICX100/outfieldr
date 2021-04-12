e8aee768d96fe0931293f2b2d234a1e43e04d4d5
vent <ventlark@gmail.com>
2021-04-12 23:52:00 +0100

    Refactor main

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,10 @@ const params = comptime [_]clap.Param(clap.Help){
     clap.parseParam("<POS>...") catch unreachable,
 };
 
+var fetch: bool = undefined;
+var lang: ?[]const u8 = undefined;
+var os: ?[]const u8 = undefined;
+
 pub fn main() anyerror!void {
     var diag: clap.Diagnostic = undefined;
     var args = clap.parse(clap.Help, &params, std.heap.page_allocator, &diag) catch |err| {
@@ -23,38 +27,34 @@ pub fn main() anyerror!void {
     };
     defer args.deinit();
 
-    const stdout = std.io.getStdOut();
-    var gpa = GeneralPurposeAllocator(.{}){};
-    var allocator = &gpa.allocator;
+    fetch = args.flag("--fetch");
+    lang = args.option("--lang");
+    os = args.option("--os");
 
     const positionals: ?[]const []const u8 = pos: {
         const pos = args.positionals();
         break :pos if (pos.len > 0) pos else null;
     };
 
+    const stdout = std.io.getStdOut();
+    var gpa = GeneralPurposeAllocator(.{}){};
+    var allocator = &gpa.allocator;
+
     if (args.flag("--help")) {
         try helpExit();
     }
 
-    const fetched = f: {
-        if (args.flag("--fetch")) {
-            try Pages.fetch(allocator);
-            if (positionals == null) std.process.exit(0);
-            _ = try stdout.writer().write("--\n");
-            break :f true;
-        } else {
-            break :f false;
-        }
-    };
+    if (fetch) {
+        try Pages.fetch(allocator);
+        if (positionals == null) std.process.exit(0);
+        _ = try stdout.writer().write("--\n");
+    }
 
     if (positionals) |pos| {
-        const lang = args.option("--lang");
-        const os = args.option("--os");
-
-        var tldr_pages = Pages.open(lang, os) catch |err| errorExit(err, fetched);
+        var tldr_pages = Pages.open(lang, os) catch |err| errorExit(err);
         defer tldr_pages.close();
 
-        const page_contents = tldr_pages.pageContents(allocator, pos) catch |err| errorExit(err, fetched);
+        const page_contents = tldr_pages.pageContents(allocator, pos) catch |err| errorExit(err);
         defer allocator.free(page_contents);
 
         const pretty_contents = try pretty.prettify(allocator, page_contents);
@@ -64,7 +64,7 @@ pub fn main() anyerror!void {
     }
 }
 
-fn errorExit(err: anyerror, fetched: bool) noreturn {
+fn errorExit(err: anyerror) noreturn {
     const stderr = std.io.getStdErr().writer();
     _ = switch (err) {
         error.AppdataNotFound => stderr.write("Appdata directory not found. Rerun with `--fetch`.\n"),
@@ -72,7 +72,7 @@ fn errorExit(err: anyerror, fetched: bool) noreturn {
         error.LanguageNotSupported => stderr.write("Language not supported.\n"),
         error.OsNotSupported => stderr.write("Operating system not supported.\n"),
         error.PageNotFound => stderr.write(msg: {
-            if (fetched)
+            if (fetch)
                 break :msg "Page doesn't exist in tldr-master. Consider contributing it!\n"
             else
                 break :msg "Page not found. Perhaps try with `--fetch`\n";
