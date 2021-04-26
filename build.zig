486b9e926eef1c84fbb30423c6dc53e414e34d14
vent <ventlark@gmail.com>
2021-04-26 18:10:49 +0100

    Remove Gyro dependency
    There have been issues bootstrapping Gyro. This might create friction
    for people trying to use this tool, especially people unfamiliar with
    Zig. I think it's best to not use Gyro in this case, and just
    submodule in the dependencies.

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
-const Builder = @import("std").build.Builder;
-const pkgs = @import("deps.zig").pkgs;
+const std = @import("std");
+const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
@@ -10,7 +10,10 @@ pub fn build(b: *Builder) void {
     exe.setBuildMode(mode);
     exe.install();
     exe.setOutputDir("bin");
-    pkgs.addAllTo(exe);
+
+    for (Packages.all) |pkg| {
+        exe.addPackage(pkg);
+    }
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -21,3 +24,43 @@ pub fn build(b: *Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
+
+const Packages = struct {
+    const clap = std.build.Pkg{
+        .name = "clap",
+        .path = "./lib/zig-clap/clap.zig",
+    };
+
+    const tar = std.build.Pkg{
+        .name = "tar",
+        .path = "./lib/tar/src/main.zig",
+    };
+
+    const hzzp = std.build.Pkg{
+        .name = "hzzp",
+        .path = "./lib/hzzp/src/main.zig",
+    };
+
+    const iguanaTLS = std.build.Pkg{
+        .name = "iguanaTLS",
+        .path = "./lib/iguanaTLS/src/main.zig",
+    };
+
+    const network = std.build.Pkg{
+        .name = "network",
+        .path = "./lib/zig-network/network.zig",
+    };
+
+    const uri = std.build.Pkg{
+        .name = "uri",
+        .path = "./lib/zig-uri/uri.zig",
+    };
+
+    const zfetch = std.build.Pkg{
+        .name = "zfetch",
+        .path = "./lib/zfetch/src/main.zig",
+        .dependencies = &[_]std.build.Pkg{ hzzp, iguanaTLS, network, uri },
+    };
+
+    const all = &[_]std.build.Pkg{ clap, tar, zfetch };
+};
